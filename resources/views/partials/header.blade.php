77085a1fc33eb53947cfb5fa7503fcab728dbc93
Nandoby <nandobiaccalli@gmail.com>
2022-09-15 13:12:00 +0200

    Adding secondary nav

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -9,7 +9,7 @@
             <form>
                 <input type="search" placeholder="Recherche des recettes ou ingrédients ..."/>
             </form>
-            <ul  id="auth">
+            <ul id="auth">
                 <li><a class="mr-4 animate" href="#">Connexion</a></li>
                 <li><a class="mr-4 animate" href="#">Inscription</a></li>
                 <li><a class="flag" href="#"><img src="flag-FR.svg" alt="Français"></a></li>
@@ -18,5 +18,26 @@
             <i id="menu-bars" class="fa-solid fa-bars"></i>
         </nav>
     </div>
-
 </header>
+<div id="nav-secondary">
+    <nav>
+        <div class="container">
+            <ul>
+                <li class="mr-4">
+                    <a class="mr-2" href="#">Recettes</a><i class="fa-solid fa-chevron-down text-red"></i>
+                    <ul>
+                        <li><a href="#">Par Catégorie</a></li>
+                        <li><a href="#">Par Saison</a></li>
+                        <li><a href="#" class="btn btn-primary">Proposer une recette</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a class="mr-2" href="">Ingrédients</a><i class="fa-solid fa-chevron-down text-red"></i>
+                    <ul>
+                        <li><a href="#">Par ordre alphabétique</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</div>
